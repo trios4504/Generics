fd659b6529d65aa741fb2aeb5296e6ad09c18c8a
trios4504 <67927315+trios4504@users.noreply.github.com>
2021-01-20 21:14:07 -0700

    Add files via upload

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<string> myClass = new MyClass<string>();
+        }
+    }
+}
+
+public class MyClass<T>
+{
+
+}
